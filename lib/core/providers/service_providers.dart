fa043a577397f99e9975c7e60fbca9f13fe6bda6
AnkarIIT <b25bs1427@iitj.ac.in>
2026-08-28 15:55:58 +0530

    feat(providers): add PyqDownloaderService provider
    - Provide PyqDownloaderService with QuestionRepository dependency
    - Enables bulk PYQ downloading from external sources

--- lib/core/providers/service_providers.dart
+++ lib/core/providers/service_providers.dart
@@ -7,6 +7,9 @@ import '../models/batch_model.dart';
 import '../services/gemini_proxy_service.dart';
 import '../services/batch_service.dart';
 import '../services/exam_checkpoint_service.dart';
+import '../services/pyq_downloader_service.dart';
+import '../database/question_repository.dart';
+import 'core_providers.dart';
 
 final biometricServiceProvider = Provider<BiometricService>((ref) {
   return BiometricService();
@@ -35,4 +38,10 @@ final activeCbtCheckpointProvider =
     FutureProvider<ExamCheckpoint?>((ref) async {
   final service = ref.watch(examCheckpointServiceProvider);
   return service.read();
+});
+
+final pyqDownloaderProvider = Provider<PyqDownloaderService>((ref) {
+  final database = ref.watch(databaseProvider);
+  final repo = QuestionRepository(database);
+  return PyqDownloaderService(repo);
 });
